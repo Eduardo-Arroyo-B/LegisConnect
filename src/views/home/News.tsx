2f3e76441fcc663c4a9385c1be643cb6334d6e96
Zenbook Eduardo <aeduardoiran@gmail.com>
2025-03-07 18:07:57 -0800

    Diseño news y creacion componentes

--- src/views/home/News.tsx
+++ src/views/home/News.tsx
@@ -1,10 +1,33 @@
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 const News = () => {
 
     return (
         <>
-            <div>Hola desde noticias</div>
+            <section className="mt-10 mr-12">
+                <label className="flex justify-center mb-6 border rounded-full border-[#996ea1] bg-[#d0a2d8]">Ultimas Noticia</label>
+                <div className="h-60 w-64 ">
+                    <Card className="w-full h-full">
+                        <CardHeader>
+                            <CardTitle>Titulo noticia</CardTitle>
+                            <CardDescription>Fecha</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p>Contenido noticia</p>
+                        </CardContent>
+                        <CardFooter>
+                            <p>Footer contenido</p>
+                        </CardFooter>
+                    </Card>
+                </div>
+            </section>
         </>
     )
 }
